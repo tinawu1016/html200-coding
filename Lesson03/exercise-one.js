607e27126f1a38864f9578ce6e3cfd5585ad0098
Tina Wu <tina710171@gmail.com>
2020-03-01 19:03:14 -0800

    reorganize doc

---
+++ Lesson03/exercise-one.js
@@ -0,0 +1,11 @@
+let name = ['kelly', 'Allison', 'James', 'Mike', 'Chris'];
+console.log(name);
+
+name.push('Ellen');
+console.log(name);
+
+name.splice(2, 1)
+console.log(name);
+
+let str = name.join(', ');
+console.log(str);
