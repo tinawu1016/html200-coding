d63d94e032fe29b2ab667a8b2f1853148d35385b
Tina Wu <tina710171@gmail.com>
2020-03-04 23:04:46 -0800

    add lesson 04

---
+++ Lesson04/exercise-1.js
@@ -0,0 +1,10 @@
+function squareIt() {
+  let newNum = prompt('Enter a number: ');
+  alert('New number: ' + (newNum * newNum));
+}
+
+function multi() {
+  let firstNumber = prompt('Enter your first number: ');
+  let secondNumber = prompt('Enter your second number: ');
+  alert('Here is your new number: ' + (firstNumber * secondNumber));
+}
